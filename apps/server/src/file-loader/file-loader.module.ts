d21f91504b1a1f02dd44224e858e613389b8e2af
Prajna <44605072+hqwuzhaoyi@users.noreply.github.com>
2023-07-07 23:57:35 +0800

    chore: delete file-loader

--- apps/server/src/file-loader/file-loader.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FileLoaderService } from './file-loader.service';
-import { FileLoaderController } from './file-loader.controller';
-
-@Module({
-  controllers: [FileLoaderController],
-  providers: [FileLoaderService]
-})
-export class FileLoaderModule {}
