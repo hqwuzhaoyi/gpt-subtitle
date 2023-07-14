326cdb6f93796466f293f0029e62360577ade18c
Prajna <44605072+hqwuzhaoyi@users.noreply.github.com>
2023-07-14 23:53:08 +0800

    chore: Put the environment variables into .env

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TranslateModule } from "./translate/translate.module";
 import { UploadModule } from "./upload/upload.module";
-import { MulterModule } from "@nestjs/platform-express";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { OsrtModule } from "./osrt/osrt.module";
@@ -12,24 +11,34 @@ import { BullModule } from "@nestjs/bull";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { FilesModule } from "./files/files.module";
 import { SharedModule } from "./shared/shared.module";
-import { StaticDirModule, StaticDirProvider } from "./static-dir.provider";
+import { StaticDirModule } from "./static-dir.provider";
+
+const {
+  REDIS_PORT = 6379,
+  REDIS_HOST = "localhost",
+  MYSQL_HOST = "localhost",
+  MYSQL_PORT = 3306,
+  MYSQL_USER = "root",
+  MYSQL_PASSWORD = "123456",
+  MYSQL_DATABASE = "gpt_subtitle",
+} = process.env;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "1241",
-      database: "gpt_subtitle",
+      host: MYSQL_HOST,
+      port: +MYSQL_PORT,
+      username: MYSQL_USER,
+      password: MYSQL_PASSWORD,
+      database: MYSQL_DATABASE,
       entities: [__dirname + "/**/*.entity{.ts,.js}"],
       synchronize: true,
     }),
     BullModule.forRoot({
       redis: {
-        host: "localhost",
-        port: 6379,
+        host: REDIS_HOST,
+        port: +REDIS_PORT,
       },
     }),
     ConfigModule.forRoot({
