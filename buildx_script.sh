a4e71d7e94579bdd83b6573da29bc3afbaea9e9f
Prajna <44605072+hqwuzhaoyi@users.noreply.github.com>
2023-07-25 00:46:28 +0800

    build: add server docker

--- buildx_script.sh
+++ buildx_script.sh
@@ -3,12 +3,17 @@
 # 设置 Docker CLI 为 experimental mode
 # export DOCKER_CLI_EXPERIMENTAL=enabled
 
+# docker build -t gpt-subtitle-web -f ./apps/web/Dockerfile --build-arg NEXT_PUBLIC_API_URL=http://192.168.2.23:3001 --build-arg API_URL=http://192.168.2.23:3001 .
+
+# docker build -t gpt-subtitle-api -f ./apps/server/Dockerfile .
+
 # 为 api 服务构建镜像并推送到 Docker registry
-API_IMAGE_NAME="hqwuzhaoyi/gpt-subtitle-web:latest"  # 你可以替换为实际的名称
-docker buildx build --platform linux/amd64,linux/arm64 -t $API_IMAGE_NAME -f ./apps/web/Dockerfile --push .
+API_IMAGE_NAME="hqwuzhaoyi/gpt-subtitle-web:latest" # 你可以替换为实际的名称
+
+docker buildx build --platform linux/amd64,linux/arm64 -t $API_IMAGE_NAME -f ./apps/web/Dockerfile --build-arg NEXT_PUBLIC_API_URL=http://localhost:3001 --build-arg API_URL=http://localhost:3001 --push .
 
 # 为 web 服务构建镜像并推送到 Docker registry
-WEB_IMAGE_NAME="hqwuzhaoyi/gpt-subtitle-api:latest"  # 你可以替换为实际的名称
+WEB_IMAGE_NAME="hqwuzhaoyi/gpt-subtitle-api:latest" # 你可以替换为实际的名称
 docker buildx build --platform linux/amd64,linux/arm64 -t $WEB_IMAGE_NAME -f ./apps/server/Dockerfile --push .
 
 echo "构建完成！"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 
-# If PGID or PUID are set, recreate user and group
-if [ ! -z "$PGID" ]; then
-    delgroup expressjs
-    addgroup --gid $PGID expressjs
-fi
-
-if [ ! -z "$PUID" ]; then
-    deluser expressjs
-    adduser --uid $PUID --gid $PGID --system --no-create-home expressjs
-fi
-
-# Ensure our application directories exist and have the correct permissions
-chown -R expressjs:expressjs /app/whisper
-chown -R expressjs:expressjs /app/uploads
-
 # Then execute the command from CMD
 exec "$@"
