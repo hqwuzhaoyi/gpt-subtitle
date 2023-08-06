1c37269233c30ecd457a6231dbd07bcbe7d5a7a8
Prajna <44605072+hqwuzhaoyi@users.noreply.github.com>
2023-08-06 10:52:49 +0800

    style: remove upload Form

--- apps/web/app/preview/upload/page.tsx
+++ apps/web/app/preview/upload/page.tsx
@@ -1,11 +1,11 @@
-import Form from "./Form";
+// import Form from "./Form";
 import { SubtitleTable } from "./SubtitleTable";
 
 export default function UploadPage() {
   return (
     <div className="hidden flex-col md:flex">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <Form />
+        {/* <Form /> */}
         <SubtitleTable />
       </div>
     </div>
