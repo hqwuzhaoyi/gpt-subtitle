de9b613c0c3489fe04ca2c0fc23acfaac0387ff2
Prajna <44605072+hqwuzhaoyi@users.noreply.github.com>
2023-08-08 13:34:26 +0800

    style: adjust Translate title

--- apps/web/app/preview/translate/page.tsx
+++ apps/web/app/preview/translate/page.tsx
@@ -5,10 +5,12 @@ export default function UploadPage() {
   return (
     <div className="hidden flex-col md:flex">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <h2 className="text-2xl font-bold tracking-tight">Translate</h2>
-        <p className="text-muted-foreground">
-          Translate subtitles using translation services.
-        </p>
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">Translate</h2>
+          <p className="text-muted-foreground">
+            Translate subtitles using translation services.
+          </p>
+        </div>
         {/* <Form /> */}
         <SubtitleTable />
       </div>
