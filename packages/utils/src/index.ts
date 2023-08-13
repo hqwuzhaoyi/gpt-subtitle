6ebf9e3edf60997d4d72da3998503272a2a626c1
Prajna <hqwuzhaoyi@gmail.com>
2023-08-13 23:31:17 +0800

    fix: change static file cant use in docker

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,7 +2,11 @@
 //   process.env.API_URL ||
 //   `http://localhost:${process.env.SERVER_PORT || 3001}`;
 
-export const baseURL = '/api/proxy'
+export const baseURL = "/api/proxy";
 
-export const staticPath = `${baseURL}${process.env.STATIC_PATH || '/static'}/`;
-export const videoDirPath = `${baseURL}${process.env.STATIC_PATH || '/static'}/video/`;
+export const staticPath = `${process.env.API_URL}${
+  process.env.STATIC_PATH || "/static"
+}/`;
+export const videoDirPath = `${process.env.API_URL}${
+  process.env.STATIC_PATH || "/static"
+}/video/`;
