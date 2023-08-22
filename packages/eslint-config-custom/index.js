40044815d3360f6b78e85b2295c27d1d49049dda
Prajna <hqwuzhaoyi@gmail.com>
2023-08-22 14:46:50 +0800

    build: change unused error to warning

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ["unused-imports"],
   rules: {
     "@typescript-eslint/no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-imports": "warn",
     "unused-imports/no-unused-vars": [
       "warn",
       {
