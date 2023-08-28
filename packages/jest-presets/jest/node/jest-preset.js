c0f20c753afb2e721a5fbb3621a86739d54d211c
Prajna <hqwuzhaoyi@gmail.com>
2023-08-28 22:24:00 +0800

    test: fix test error

--- packages/jest-presets/jest/node/jest-preset.js
+++ packages/jest-presets/jest/node/jest-preset.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+  testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   modulePathIgnorePatterns: [
     "<rootDir>/test/__fixtures__",
--- packages/ui/src/__tests__/index.test.tsx
+++ packages/ui/src/__tests__/index.test.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom/client';
 import { CounterButton } from "../CounterButton";
 
 describe("CounterButton", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<CounterButton />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const root = ReactDOM.createRoot(div);
+    root.render(<CounterButton />);
+    root.unmount();
   });
 });
