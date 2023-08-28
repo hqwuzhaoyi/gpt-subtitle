6477416cff227f5671edec1fdcafc65a07c958cc
Prajna <hqwuzhaoyi@gmail.com>
2023-08-28 22:24:18 +0800

    test: add utils package unit test

---
+++ packages/utils/src/__tests__/index.test.ts
@@ -0,0 +1,53 @@
+// Assuming your config is in a file called config.js
+import { baseURL } from "../index";
+
+describe("Configuration", () => {
+  let originalEnv;
+
+  beforeEach(() => {
+    // 存储当前的环境变量
+    originalEnv = { ...process.env };
+    jest.resetModules();
+  });
+
+  afterEach(() => {
+    // 恢复原始环境变量
+    process.env = originalEnv;
+  });
+
+  it("should set baseURL to /api/proxy", () => {
+    expect(baseURL).toBe("/api/proxy");
+  });
+
+  it("should set staticPath based on environment variables", () => {
+    // Mock environment variables
+    Object.defineProperty(process.env, "API_URL", {
+      value: "http://test.com",
+      writable: true,
+    });
+    Object.defineProperty(process.env, "STATIC_PATH", {
+      value: "/test_static",
+      writable: true,
+    });
+
+    const config = require("../index"); // 替换为你的模块路径
+
+    expect(config.staticPath).toBe("http://test.com/test_static/");
+  });
+
+  it("should set videoDirPath based on environment variables", () => {
+    // Mock environment variables
+    Object.defineProperty(process.env, "API_URL", {
+      value: "http://test.com",
+      writable: true,
+    });
+    Object.defineProperty(process.env, "STATIC_PATH", {
+      value: "/test_static",
+      writable: true,
+    });
+
+    const config = require("../index"); // 替换为你的模块路径
+
+    expect(config.videoDirPath).toBe("http://test.com/test_static/video/");
+  });
+});
