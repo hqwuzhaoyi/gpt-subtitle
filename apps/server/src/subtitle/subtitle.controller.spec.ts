76e36b90e65a6692f0e77a271e1f4a983d2ac733
Prajna <hqwuzhaoyi@gmail.com>
2023-09-12 00:45:28 +0800

    test: add subtitle test

--- apps/server/src/subtitle/subtitle.controller.spec.ts
+++ apps/server/src/subtitle/subtitle.controller.spec.ts
@@ -1,20 +1,86 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SubtitleController } from './subtitle.controller';
-import { SubtitleService } from './subtitle.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { SubtitleController } from "./subtitle.controller";
+import { SubtitleService } from "./subtitle.service";
 
-describe('SubtitleController', () => {
+describe("SubtitleController", () => {
   let controller: SubtitleController;
+  let service: SubtitleService;
 
   beforeEach(async () => {
+    const mockSubtitleService = {
+      create: jest.fn(),
+      findAll: jest.fn(),
+      findOne: jest.fn(),
+      update: jest.fn(),
+      remove: jest.fn(),
+      uploadFile: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SubtitleController],
-      providers: [SubtitleService],
+      providers: [{ provide: SubtitleService, useValue: mockSubtitleService }],
     }).compile();
 
     controller = module.get<SubtitleController>(SubtitleController);
+    service = module.get<SubtitleService>(SubtitleService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  // Add more test cases
+
+  // Continue from the previous code block...
+
+  it("should create subtitle", async () => {
+    const createSubtitleDto = {
+      /* your DTO here */
+    };
+    (service.create as jest.Mock).mockResolvedValue("someValue");
+
+    expect(await controller.create(createSubtitleDto)).toBe("someValue");
+  });
+
+  it("should find all subtitles", async () => {
+    (service.findAll as jest.Mock).mockResolvedValue([
+      "someValue1",
+      "someValue2",
+    ]);
+
+    expect(await controller.findAll()).toEqual(["someValue1", "someValue2"]);
+  });
+
+  it("should find one subtitle", async () => {
+    const id = "1";
+    (service.findOne as jest.Mock).mockResolvedValue("someValue");
+
+    expect(await controller.findOne(id)).toBe("someValue");
+  });
+
+  it("should update subtitle", async () => {
+    const id = "1";
+    const updateSubtitleDto = {
+      /* your DTO here */
+    };
+    (service.update as jest.Mock).mockResolvedValue("someValue");
+
+    expect(await controller.update(id, updateSubtitleDto)).toBe("someValue");
+  });
+
+  it("should remove subtitle", async () => {
+    const id = "1";
+    (service.remove as jest.Mock).mockResolvedValue("someValue");
+
+    expect(await controller.remove(id)).toBe("someValue");
+  });
+
+  it("should upload subtitle file", async () => {
+    const file = {
+      /* your file mock here */
+    };
+    (service.uploadFile as jest.Mock).mockResolvedValue("someValue");
+
+    expect(await controller.uploadFile(file)).toBe("someValue");
   });
 });
--- apps/server/src/subtitle/subtitle.service.spec.ts
+++ apps/server/src/subtitle/subtitle.service.spec.ts
@@ -1,18 +1,57 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SubtitleService } from './subtitle.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { SubtitleService } from "./subtitle.service";
+import { FilesService } from "@/files/files.service";
+import { WatchService } from "@/files/watch/watch.service";
 
-describe('SubtitleService', () => {
+describe("SubtitleService", () => {
   let service: SubtitleService;
+  let filesService: FilesService;
+  let watchService: WatchService;
 
   beforeEach(async () => {
+    const mockFilesService = {
+      findSubtitleFiles: jest.fn(),
+      findSubtitleFile: jest.fn(),
+      removeWithPath: jest.fn(),
+    };
+
+    const mockWatchService = {
+      addFileToDB: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SubtitleService],
+      providers: [
+        SubtitleService,
+        { provide: FilesService, useValue: mockFilesService },
+        { provide: WatchService, useValue: mockWatchService },
+        { provide: "STATIC_DIR", useValue: "/static" },
+      ],
     }).compile();
 
     service = module.get<SubtitleService>(SubtitleService);
+    filesService = module.get<FilesService>(FilesService);
+    watchService = module.get<WatchService>(WatchService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
+
+  it("should create a subtitle", () => {
+    const result = service.create({
+      /* createSubtitleDto */
+    });
+    expect(result).toEqual("This action adds a new subtitle");
+  });
+
+  it("should find all subtitles", async () => {
+    (filesService.findSubtitleFiles as jest.Mock).mockResolvedValue([
+      { filePath: "file1" },
+      { filePath: "file2" },
+    ]);
+    const result = await service.findAll();
+    expect(result).toHaveLength(2);
+  });
+
+  // ... More tests for other methods
 });
