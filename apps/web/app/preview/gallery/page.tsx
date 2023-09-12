0de6aef56171d687acba5761d9eb724435323bd6
Prajna <hqwuzhaoyi@gmail.com>
2023-09-12 14:23:39 +0800

    fix: web app build error

--- apps/web/app/preview/gallery/page.tsx
+++ apps/web/app/preview/gallery/page.tsx
@@ -41,41 +41,48 @@ export default async function TaskPage() {
     });
   };
 
-  const data = await queryData();
+  try {
+    const data = await queryData();
 
-  return (
-    <>
-      <div className=" h-full flex-1 flex-col space-y-8 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Whisper</h2>
-            <p className="text-muted-foreground">
-              Use Whisper to output srt files for your videos.
-            </p>
-          </div>
-          <div className="flex items-center space-x-2">{/* <UserNav /> */}</div>
-        </div>
-
-        <Separator className="my-4" />
-
-        <div className="relative">
-          <ScrollArea className="h-[600px] w-full">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-14">
-              {data.map((album) => (
-                <AlbumArtwork
-                  key={album.name}
-                  album={album}
-                  className="w-[220px] h-[120px]"
-                  aspectRatio="portrait"
-                  width={220}
-                  height={120}
-                />
-              ))}
+    return (
+      <>
+        <div className=" h-full flex-1 flex-col space-y-8 p-8 md:flex">
+          <div className="flex items-center justify-between space-y-2">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">Whisper</h2>
+              <p className="text-muted-foreground">
+                Use Whisper to output srt files for your videos.
+              </p>
             </div>
-            <ScrollBar />
-          </ScrollArea>
+            <div className="flex items-center space-x-2">
+              {/* <UserNav /> */}
+            </div>
+          </div>
+
+          <Separator className="my-4" />
+
+          <div className="relative">
+            <ScrollArea className="h-[600px] w-full">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-14">
+                {data.map((album) => (
+                  <AlbumArtwork
+                    key={album.name}
+                    album={album}
+                    className="w-[220px] h-[120px]"
+                    aspectRatio="portrait"
+                    width={220}
+                    height={120}
+                  />
+                ))}
+              </div>
+              <ScrollBar />
+            </ScrollArea>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } catch (error) {
+    console.debug(error);
+    return <div>error</div>;
+  }
 }
