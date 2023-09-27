c6232848f1996d18382c5872ac0c5fb9a413d7d7
Prajna <hqwuzhaoyi@gmail.com>
2023-09-27 22:59:29 +0800

    test: Improve OSRT Controller Unit Test Supplement

--- apps/server/src/osrt/osrt.controller.spec.ts
+++ apps/server/src/osrt/osrt.controller.spec.ts
@@ -2,22 +2,40 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { OsrtController } from "./osrt.controller";
 import { OsrtService } from "./osrt.service";
 import { CreateOsrtDto } from "./dto/create-osrt.dto";
+import { Subject } from "rxjs";
+
+const req = { headers: { host: "localhost" } } as any;
 
 describe("OsrtController", () => {
   let controller: OsrtController;
   let service: OsrtService;
+  let mockSubject: Subject<any>;
 
   beforeEach(async () => {
     const mockService = {
       create: jest.fn(),
       list: jest.fn(),
       findAudios: jest.fn(),
-      // ...其他方法
+      findAllModels: jest.fn(),
+      findOne: jest.fn(),
+      stop: jest.fn(),
+      autoStart: jest.fn(),
+      getActiveJobs: jest.fn(),
+      clearAllJobs: jest.fn(),
+      terminateAllJobs: jest.fn(),
+      translate: jest.fn(),
+      createJobs: jest.fn(),
+      update: jest.fn(),
+      remove: jest.fn(),
     };
+    mockSubject = new Subject<any>();
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OsrtController],
-      providers: [{ provide: OsrtService, useValue: mockService }],
+      providers: [
+        { provide: OsrtService, useValue: mockService },
+        { provide: "EVENT_SUBJECT", useValue: mockSubject },
+      ],
     }).compile();
 
     controller = module.get<OsrtController>(OsrtController);
@@ -26,6 +44,16 @@ describe("OsrtController", () => {
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("post", () => {
+    it("should create an osrt", async () => {
+      const expectedResult = "This action adds a new osrt";
+
+      (service.create as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.create(req)).toBe(expectedResult);
+    });
   });
 
   describe("create", () => {
@@ -59,5 +87,179 @@ describe("OsrtController", () => {
     });
   });
 
-  // ...其他方法的测试
+  describe("findAudios", () => {
+    it("should findAudios", async () => {
+      const expectedResult = [
+        /* some list */
+      ];
+
+      (service.findAudios as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.findAudios()).toBe(expectedResult);
+      expect(service.findAudios).toHaveBeenCalled();
+    });
+  });
+
+  describe("findAllModels", () => {
+    it("should findAllModels", async () => {
+      const expectedResult = [
+        /* some list */
+      ];
+
+      (service.findAllModels as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.findAllModels()).toBe(expectedResult);
+      expect(service.findAllModels).toHaveBeenCalled();
+    });
+  });
+
+  describe("findOne", () => {
+    it("should findOne", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.findOne as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.findOne("ln", "file")).toBe(expectedResult);
+      expect(service.findOne).toHaveBeenCalled();
+    });
+  });
+
+  describe("stop", () => {
+    it("should stop", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.stop as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.stop("processingJobId")).toBe(expectedResult);
+      expect(service.stop).toHaveBeenCalled();
+    });
+  });
+
+  describe("autoStart", () => {
+    it("should autoStart", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.autoStart as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.autoStart("ln", "model")).toBe(expectedResult);
+      expect(service.autoStart).toHaveBeenCalled();
+    });
+  });
+
+  describe("getActiveJobs", () => {
+    it("should getActiveJobs", async () => {
+      const expectedResult = [
+        /* some list */
+      ];
+
+      (service.getActiveJobs as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.currentJobs()).toBe(expectedResult);
+      expect(service.getActiveJobs).toHaveBeenCalled();
+    });
+  });
+
+  describe("clearAllJobs", () => {
+    it("should clearAllJobs", async () => {
+      const expectedResult = [
+        /* some list */
+      ];
+
+      (service.clearAllJobs as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.clearAllJobs()).toBe(expectedResult);
+      expect(service.clearAllJobs).toHaveBeenCalled();
+    });
+  });
+
+  describe("terminateAllJobs", () => {
+    it("should terminateAllJobs", async () => {
+      const expectedResult = [
+        /* some list */
+      ];
+
+      (service.terminateAllJobs as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.terminateAllJobs()).toBe(expectedResult);
+      expect(service.terminateAllJobs).toHaveBeenCalled();
+    });
+  });
+
+  describe("translate", () => {
+    it("should translate", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.translate as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.translate("ln", "id", "model", 1, "video")).toBe(
+        expectedResult
+      );
+      expect(service.translate).toHaveBeenCalled();
+    });
+  });
+
+  describe("translatePost", () => {
+    it("should translatePost", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.translate as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(
+        await controller.translatePost("id", "ln", "model", 1, "video")
+      ).toBe(expectedResult);
+      expect(service.translate).toHaveBeenCalled();
+    });
+  });
+  describe("update", () => {
+    it("should update", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.update as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.update("id", {})).toBe(expectedResult);
+    });
+  });
+  describe("remove", () => {
+    it("should remove", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.remove as jest.Mock).mockResolvedValue(expectedResult);
+
+      expect(await controller.remove("id")).toBe(expectedResult);
+    });
+  });
+
+  describe("createJobs", () => {
+    it("should createJobs", async () => {
+      const expectedResult = {
+        /* some object */
+      };
+
+      (service.createJobs as jest.Mock).mockResolvedValue(expectedResult);
+      const jobs = [
+        {
+          id: "string",
+          language: "string",
+          model: "string",
+        },
+      ];
+
+      expect(await controller.createJobs(jobs)).toBe(expectedResult);
+      expect(service.createJobs).toHaveBeenCalled();
+    });
+  });
 });
