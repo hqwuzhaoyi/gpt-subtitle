b79fc64f736c2ebdc722d032431144b0aacc66e5
Prajna <hqwuzhaoyi@gmail.com>
2023-11-08 23:43:44 +0800

    feat: add settings page

---
+++ apps/web/app/settings/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+// import Link from "next/link";
+// import { ChevronRight } from "lucide-react";
+
+export default function ExamplesLayout({ children }: ExamplesLayoutProps) {
+  return (
+    <>
+      <div className="container relative p-10">
+        <section>
+          <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow-xl">
+            123
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
