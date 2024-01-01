07691c68d0856e20b5543640765af55f1d76ce27
Prajna <hqwuzhaoyi@gmail.com>
2024-01-02 00:27:22 +0800

    Remove experimental serverActions from next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,9 +2,6 @@ require("dotenv").config({ path: "../../.env" });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
