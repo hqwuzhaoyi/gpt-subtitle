4a5f30c44ddf3ad45894cc5876ef7987bbb1983e
Prajna <hqwuzhaoyi@gmail.com>
2024-01-22 23:54:23 +0800

    Refactor global validation pipe options in main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,7 +8,14 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 async function bootstrap2() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
   console.debug("SERVER_PORT", process.env.SERVER_PORT);
   await app.listen(process.env.SERVER_PORT);
 }
