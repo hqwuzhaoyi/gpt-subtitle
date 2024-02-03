df9fb5e028865e655f83becec309de1933b1a825
zhaoyi.wu <zhaoyi.wu@amh-group.com>
2024-02-03 16:54:02 +0800

    Add generateStaticParams function and set request locale

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -8,6 +8,12 @@ import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Providers } from "./providers";
 import { StyleSwitcher } from "@/components/style-switcher";
 import { NextIntlClientProvider, useMessages } from "next-intl";
+import { locales } from "@/lib/navigation";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
 
 export default function LocaleLayout({
   children,
@@ -16,7 +22,9 @@ export default function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
+  unstable_setRequestLocale(locale);
   const messages = useMessages();
+
   return (
     <html lang={locale}>
       <body
